5e3bb00cf57222a7b3b7d5b20c9294b0893ff230
Stanley Lee <stanleylee91776@gmail.com>
2018-11-17 15:01:59 -0800

    cats are dead

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from newsapi import NewsApiClient
 import weight
-userweight = weight.weighter("CNN", 10)
-print(userweight)
+
+newsapi = NewsApiClient(api_key='c38f8442c3f14c59acd996b41d7f4d4c')
+sources = "cnn, the-new-york-times, bbc-news, the-guardian-uk, associated-press, usa-today, the-economist, the-hill, fortune, the-wall-street-journal"
+sourcesarray = sources.split(", ")
+
+top_economy = newsapi.get_top_headlines(q='economy', sources=sources)
+top_politics = newsapi.get_top_headlines(q='politics', sources=sources)
+top_sport = newsapi.get_top_headlines(q='sports', sources=sources)
+top_tech = newsapi.get_top_headlines(q='technology', sources=sources)
+
+# print(top_tech)
+# print(weight.weighter("cnn", 7))
