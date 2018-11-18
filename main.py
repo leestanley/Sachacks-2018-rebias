0c9030df037e031131f7f58f1a173fba08a4d361
Jeffrey Ha <jha@ucsd.edu>
2018-11-17 20:25:11 -0800

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from newsapi import NewsApiClient
 from flask import Flask, url_for, render_template, request, jsonify, session
-import weight
+import weight, os
 
 
 from datascience import Table
@@ -16,13 +16,10 @@ app = Flask(__name__)
 newsapi = NewsApiClient(api_key='c38f8442c3f14c59acd996b41d7f4d4c')
 
 # session.clear() (if need to clear session)
-app.secret_key = "f6438f71864f3da4caa96d2dfe804edb"
 
+currentWeight = 50
 sources = "cnn, the-new-york-times, bbc-news, the-guardian-uk, associated-press, usa-today, the-economist, the-hill, fortune, the-wall-street-journal"
 sourcesarray = sources.split(", ")
-global currWeight
-currWeight = 50
-
 
 def get_news_by_category(category):
   top_news = []
@@ -54,17 +51,25 @@ def get_news_by_category(category):
     top_news.append(newsapi.get_top_headlines(q='phone', sources=sources))
     top_news.append(newsapi.get_top_headlines(q='Silicon', sources=sources))
     top_news.append(newsapi.get_top_headlines(q='Samsung', sources=sources))
+  
+  
   final = []
   for y in top_news:
     for x in y['articles']:
       final.append({'title': x['title'], 'url': x['url'], 'image': x['urlToImage'], 'id': x['source']['id']})
-  return final
+  return weightranker(currentWeight, final)
 
 
-def get_user_weight(source, rating):
-  currWeight = weight.weighter(currWeight, source, rating)
-  session["currWeight"] = currWeight
-  return currWeight
+def get_user_weight(source, rating, currWeight = 50):
+  cWeight = weight.weighter(currWeight, source, rating)
+  if (cWeight < 0):
+    cWeight = 0
+  if (cWeight > 100):
+    cWeight = 100
+  
+  currentWeight = cWeight
+  session["currentWeight"] = cWeight
+  return currentWeight
 
 
 def weight_from_mean(currweight, source):
@@ -79,36 +84,41 @@ def weightranker(currweight, top_news):  # when get_news_by_category is called, 
           top_news[i], top_news[j] = top_news[j], top_news[i]
   return top_news[:5]
 
-
-@app.before_request
-def pre_session():
-  session.permanent = True
-
-
-@app.route("/", methods=["GET"])
+@app.route("/")
 def home():
-  if session["currWeight"]:
-    currWeight = session["currWeight"]
-    print("session loaded")
+  print(session.get("currentWeight"))
+  return "hi"
 
 
 @app.route("/api/updateWeight")
 def updateWeight():
+  if not ("currentWeight" in session):
+    session["currentWeight"] = 50
+  else:
+    currentWeight = session["currentWeight"]
+  
   if request.method == "GET":
     source = request.args.get("source")
     user_rating = request.args.get("user_rating")
 
-  return get_user_weight(source, user_rating)
+  new_weight = get_user_weight(source, int(user_rating), currentWeight)
+  session["currentWeight"] = new_weight
+  return str(new_weight)
 
 
 @app.route("/api/getNews")
 def getNews():
+  if not ("currentWeight" in session):
+    session["currentWeight"] = 50
+  else:
+    currentWeight = session["currentWeight"]
+  
   if request.method == "GET":
     category = request.args.get("category")
 
     if (category):
       return jsonify(get_news_by_category(category))
 
-
 if __name__ == "__main__":
+  app.secret_key = os.urandom(24)
   app.run(port=5001)
