0f595acebdcca6aca1b39599e21cfa190af0d075
Stanley Lee <stanleylee91776@gmail.com>
2018-11-17 16:11:46 -0800

    updated main and weight

--- main.py
+++ main.py
@@ -5,16 +5,27 @@ newsapi = NewsApiClient(api_key='c38f8442c3f14c59acd996b41d7f4d4c')
 sources = "cnn, the-new-york-times, bbc-news, the-guardian-uk, associated-press, usa-today, the-economist, the-hill, fortune, the-wall-street-journal"
 sourcesarray = sources.split(", ")
 
-top_economy = newsapi.get_top_headlines(q='economy', sources=sources)
-top_politics = newsapi.get_top_headlines(q='politics', sources=sources)
-top_sport = newsapi.get_top_headlines(q='sports', sources=sources)
-top_tech = newsapi.get_top_headlines(q='technology', sources=sources)
 
-categories = [top_economy, top_politics, top_sport, top_tech]
-final = []
-for x in categories:
-    for y in x['articles']:
-        final.append((y['title'], y['url']))
+def get_news_by_category(category):
+    # top_economy = newsapi.get_top_headlines(q='economy', sources=sources)
+    # top_politics = newsapi.get_top_headlines(q='politics', sources=sources)
+    # top_sport = newsapi.get_top_headlines(q='sports', sources=sources)
+    # top_tech = newsapi.get_top_headlines(q='technology', sources=sources)
+    top_news = newsapi.get_top_headlines(q=category, sources=sources)
 
-print(final)
-# print(weight.weighter("cnn", 7))
+# categories = [top_economy, top_politics, top_sport, top_tech]
+    final = []
+
+# for x in categories:
+    for x in top_news['articles']:
+        final.append([x['title'], x['url'], x['source']['id']])
+    return final
+# currweight = 50
+# for x in final:
+#     currweight = weight.weighter(currweight, x[2], user_rating)
+
+
+# print(currweight)
+a = get_news_by_category('politics')
+for x in a:
+    print(x)
--- weight.py
+++ weight.py
@@ -4,12 +4,12 @@ bias = Table().read_table("bias.csv").select("News Source", "Horizontal Rank")
 userweight = 50
 
 
-def weighter(source, rating):
+def weighter(currweight, source, rating):
     horirank = bias.where("News Source", source).column("Horizontal Rank").item(0)
     # print(horirank)
-    return userweight + (((rating - 5) / 10) * horirank)
+    return currweight + (((rating - 5) / 10) * horirank)
 
 
 #userweight = weighter("LA Times", 2)
 #userweight = weighter("LA Times", 10)
-#print(userweight)
+# print(userweight)
