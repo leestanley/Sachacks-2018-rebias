8deab272b32f7a965285200bb09ff07b98104106
Stanley Lee <stanleylee91776@gmail.com>
2018-11-18 01:48:40 -0800

    update

--- plottest/graph.py
+++ plottest/graph.py
@@ -1,16 +1,24 @@
 import matplotlib.pyplot as plt
+import matplotlib.style as style
 import io
 import base64
+import numpy as np
 
 
-def build_graph(x_coordinates, y_coordinates):
+def build_graph(ratings, biasscore):
+    style.use('fivethirtyeight')
+    ratings = np.arange(0, ratings)
+    plt.locator_params(integer=True)
     img = io.BytesIO()
-    plt.plot(x_coordinates, y_coordinates)
+    plt.tight_layout()
+    plt.plot(ratings, biasscore, color='m')
+    plt.xlabel('Amount of Ratings')
+    plt.ylabel('Bias Score')
+    plt.title("Ratings Plotted Against Bias Score")
+    plt.tight_layout()
+    plt.ylim((0, 100))
     plt.savefig(img, format='png')
     img.seek(0)
     graph_url = base64.b64encode(img.getvalue()).decode()
     plt.close()
     return 'data:image/png;base64,{}'.format(graph_url)
-
-
-build_graph(1, 1)
